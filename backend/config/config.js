1e3f05869177ec963e348d2ef6ef3e6e03f241e0
Sarampais <152285067+Sarampais@users.noreply.github.com>
2024-06-17 22:26:35 +0100

    Delete backend/config directory

--- backend/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: "projetodeprogramacaosaraetatiana24",
-    timer: 600000, //10 minutos
-  };
--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require("sequelize");
-const conexao = new sequelize("projetopratico", "root", "", {
-  host: "localhost",
-  port: 3308,
-  dialect: "mysql",
-});
-
-module.exports = conexao;
